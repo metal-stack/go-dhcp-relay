a5b27ac5516c53517dfb0c02f79b9846d75d5ba0
Ilja Rotar <kontakt@ilja-rotar.de>
2025-04-07 17:02:35 +0200

    full dora exchange works in the lab

--- server/handlers.go
+++ server/handlers.go
@@ -15,15 +15,19 @@ func (s *Server) handlePacket(packet *dhcpv4.DHCPv4) error {
 	case dhcpv4.MessageTypeOffer:
 		return s.handleMessageTypeOffer(packet)
 	case dhcpv4.MessageTypeRequest:
+		return s.handleMessageTypeRequest(packet)
 	case dhcpv4.MessageTypeAck:
+		return s.handleMessageTypeAck(packet)
 	}
+
+	// TODO: handle all types of messages
 
 	return nil
 }
 
 func (s *Server) handleMessageTypeDiscover(packet *dhcpv4.DHCPv4) error {
 	s.log.Debug("handle discover", "packet", packet.Summary())
-	if packet.HopCount >= 3 { // make this value configurable, rfc maximum is 16
+	if packet.HopCount >= 3 { // TODO: make this value configurable, rfc maximum is 16
 		return fmt.Errorf("maximum hop count exceeded, dropping packet")
 	}
 	packet.HopCount++
@@ -53,6 +57,50 @@ func (s *Server) handleMessageTypeDiscover(packet *dhcpv4.DHCPv4) error {
 
 func (s *Server) handleMessageTypeOffer(packet *dhcpv4.DHCPv4) error {
 	s.log.Debug("handle offer", "packet", packet.Summary())
+
+	broadcastAddresses, err := getBroadcastAddresses(s.config.Interface)
+	if err != nil {
+		return fmt.Errorf("failed to get broadcast addresses for interface:%w", err)
+	}
+
+	for _, ip := range broadcastAddresses {
+		addr := &net.UDPAddr{
+			IP:   ip,
+			Port: dhcpv4.ClientPort,
+		}
+
+		n, err := s.sendTo(packet, addr)
+		if err != nil {
+			return fmt.Errorf("failed to send packet to %s:%w", addr, err)
+		}
+		s.log.Debug("packet sent to client", "bytes sent", n, "address", addr, "packet", packet.Summary())
+	}
+
+	return nil
+}
+
+func (s *Server) handleMessageTypeRequest(packet *dhcpv4.DHCPv4) error {
+	s.log.Debug("handle request", "packet", packet.Summary())
+
+	packet.GatewayIPAddr = net.ParseIP(s.config.GatewayAddress)
+
+	serverIP := packet.ServerIdentifier()
+	addr := &net.UDPAddr{
+		IP:   serverIP,
+		Port: dhcpv4.ServerPort,
+	}
+
+	n, err := s.sendTo(packet, addr)
+	if err != nil {
+		return fmt.Errorf("failed to send packet to server %s:%w", serverIP, err)
+	}
+	s.log.Debug("packet sent to server", "bytes sent", n, "server address", serverIP, "packet", packet.Summary())
+
+	return nil
+}
+
+func (s *Server) handleMessageTypeAck(packet *dhcpv4.DHCPv4) error {
+	s.log.Debug("handle acknowledgment", "packet", packet.Summary())
 
 	broadcastAddresses, err := getBroadcastAddresses(s.config.Interface)
 	if err != nil {
